c0b29bc44525f23f0c35003b04964b7a2d0fb64d
mohammad87115 <mohammad87115@gmail.com>
2024-10-31 23:45:47 +0330

    jagh

--- routers/main.js
+++ routers/main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('im alive')
+    res.send('I\'m Alive')
 })
 
 module.exports = {
---
+++ routers/receive.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const fs = require('node:fs');
+const { join } = require('node:path');
+const router = express.Router()
+
+/*
+    Example usage:
+
+    /receive/mahsa-normal
+    returns all
+
+    /receive/mahsa-normal?amount=100
+    returns first 100 configs
+*/
+router.get('/:name', async (req, res) => {
+    const v2rayFiles = fs.readdirSync('proxies/v2ray');
+    const fileName = v2rayFiles.find(file => (file === req.params.name) || (file.split('.')[0] === req.params.name));
+    const fileContent = fs.readFileSync(`proxies/v2ray/${fileName}`, { encoding: 'utf8' });
+    const configs = fileContent.split('\n');
+    const amount = Number(req.query.amount) || Number(req.query.limit) || Number(req.query.count) || -1;
+    const sliced = configs.slice(0, amount);
+    const joined = sliced.join('\n');
+    res.set('Content-Type', 'text/plain');
+    if (req.query.decrypted == '') {
+        res.status(200).send(joined);
+    } else {
+        const encrypted = btoa(joined);
+        res.status(200).send(encrypted);
+    }
+})
+
+router.get('/', async (req,res) => {
+    const v2rayFiles = fs.readdirSync('proxies/v2ray');
+    const mapped = v2rayFiles.map(file => file.split('.')[0] || file);
+    const joined = mapped.join(', ');
+    res.status(200).send(`available endpoints: ${joined}`);
+})
+
+module.exports = {
+    path: '/receive',
+    router: router
+}
