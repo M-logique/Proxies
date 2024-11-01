777a5091c59e93a84a9fef3e10e888ade5e8fc08
mohammad87115 <mohammad87115@gmail.com>
2024-11-01 12:26:15 +0330

    Added /discord route and changed main routes to respond to all kinds of requests

--- routers/main.js
+++ routers/main.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.all('/', (req, res) => {
     res.send('I\'m Alive')
+})
+
+router.all('/discord', (req, res) => {
+    res.redirect('https://discord.gg/uqRESgqZSQ')
 })
 
 module.exports = {
