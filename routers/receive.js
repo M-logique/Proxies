b7b5bd9910485fca72521a2b3d64430fb9571046
mohammad87115 <mohammad87115@gmail.com>
2024-11-01 12:02:39 +0330

    fixed some typos and removed the debugging log

--- routers/receive.js
+++ routers/receive.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const fs = require('node:fs');
-const { join } = require('node:path');
 const router = express.Router()
 
 /*
@@ -18,7 +17,6 @@ router.get('/:name', async (req, res) => {
     const fileContent = fs.readFileSync(`proxies/v2ray/${fileName}`, { encoding: 'utf8' });
     const configs = fileContent.split('\n');
     const protocolFiltered = configs.filter(config => !req.query.protocol || config.startsWith(req.query.protocol))
-    console.log(protocolFiltered.length)
     const amount = Number(req.query.amount) || Number(req.query.limit) || Number(req.query.count) || -1;
     const sliced = protocolFiltered.slice(0, amount);
     const joined = sliced.join('\n');
@@ -29,14 +27,14 @@ router.get('/:name', async (req, res) => {
         const encrypted = btoa(unescape(encodeURIComponent(joined)));
         res.status(200).send(encrypted);
     }
-})
+});
 
 router.get('/', async (req,res) => {
     const v2rayFiles = fs.readdirSync('proxies/v2ray');
     const mapped = v2rayFiles.map(file => file.split('.')[0] || file);
     const joined = mapped.join(', ');
     res.status(200).send(`available endpoints: ${joined}`);
-})
+});
 
 module.exports = {
     path: '/receive',
