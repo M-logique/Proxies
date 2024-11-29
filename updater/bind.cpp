9f19d77ff8b714c14f5a512aa4668bf1e5b86c5c
M-logique <lmy615730@gmail.com>
2024-11-29 15:33:38 +0330

    Update bind.cpp

--- updater/bind.cpp
+++ updater/bind.cpp
@@ -1,26 +1,27 @@
 #include <pybind11/pybind11.h>
 #include <string>
-#include "updater.h"
 using namespace std;
 
 namespace py = pybind11;
 
+extern "C" {
+    char* FetchResources();
+    char* FetchTGChannels(const char* data); // Update to accept const char*
+}
 
 string fetch_resources() {
     char* result = FetchResources();
     string resources(result);
-    free(result); 
+    free(result); // Ensure FetchResources allocates memory with malloc/strdup
     return resources;
 }
 
-
 string fetch_tg_channels(const string& data) {
-    char* result = FetchTGChannels(const_cast<char*>(data.c_str()));
+    char* result = FetchTGChannels(data.c_str());
     string channels(result);
     free(result); 
     return channels;
 }
-
 
 PYBIND11_MODULE(resources, m) {
     m.doc() = "Python bindings for resource-fetching functions";
