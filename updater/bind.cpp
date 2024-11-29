7b8aeec45191c45d9b84ad8070621a60a9d7013d
M-logique <lmy615730@gmail.com>
2024-11-29 15:56:31 +0330

    Update bind.cpp

--- updater/bind.cpp
+++ updater/bind.cpp
@@ -4,15 +4,21 @@ using namespace std;
 
 namespace py = pybind11;
 
+/*
+This Python module interfaces with Go functions via C bindings.
+It allows Python programs to call Go code that performs resource-fetching and other operations.
+*/
+
+
 extern "C" {
     char* FetchResources();
-    char* FetchTGChannels(const char* data); // Update to accept const char*
+    char* FetchTGChannels(const char* data);
 }
 
 string fetch_resources() {
     char* result = FetchResources();
     string resources(result);
-    free(result); // Ensure FetchResources allocates memory with malloc/strdup
+    free(result); 
     return resources;
 }
 
@@ -24,7 +30,7 @@ string fetch_tg_channels(const string& data) {
 }
 
 PYBIND11_MODULE(resources, m) {
-    m.doc() = "Python bindings for resource-fetching functions";
+    m.doc() = "Python bindings for resource-fetching operations";
 
     m.def("fetch_resources", &fetch_resources, "Fetches resources and returns them as a JSON string.");
     m.def("fetch_tg_channels", &fetch_tg_channels, py::arg("data"), "Fetches Telegram channels using input data.");
