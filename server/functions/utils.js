81ab03b16f7cac6f97641dc732511e70fc14dd2d
M-logique <lmy615730@gmail.com>
2024-12-05 08:50:25 +0330

    Update utils.js

--- server/functions/utils.js
+++ server/functions/utils.js
@@ -12,10 +12,13 @@ module.exports = {
         res.redirect(targetUrl);
     },
     setHeaders: function (res, subName) {
-        res.set('Profile-Title', subName)
-        res.set('profile-update-interval', 6)
-        res.set('profile-web-page-url', "https://github.com/M-logique/Proxies")
-        res.set('support-url', "https://github.com/M-logique/Proxies")
-        res.set('Content-Type', 'text/plain')
+        res.set('profile-title', subName)
+        res.set("upload", 0);
+        res.set("download", 0);
+        res.set("total", 10737418240000000)
+        res.set('profile-update-interval', 6);
+        res.set('profile-web-page-url', "https://github.com/M-logique/Proxies");
+        res.set('support-url', "https://github.com/M-logique/Proxies");
+        res.set('Content-Type', 'text/plain');
     }
 };
