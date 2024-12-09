08bdbc960661c41be5b1d40905bfb72370727275
SextyNine <mohammad87115@gmail.com>
2024-12-09 18:53:27 +0330

    - Added support for checked configs in webserver - Changed Git: M-logique/Proxies to Github: M-logique/Proxies because the repository is hosted by github - Added pnpm lock file to gitignore

--- server/functions/utils.js
+++ server/functions/utils.js
@@ -14,7 +14,6 @@ module.exports = {
     setHeaders: function (res, subName) {
         
         res.set('profile-title', `base64:${this.b64encode(subName)}`);
-        res.set("subscription-userinfo", "upload=0; download=0; total=10737418240000000; expire=2546249531");
         res.set('profile-update-interval', 6);
         res.set('profile-web-page-url', "https://github.com/M-logique/Proxies");
         res.set('support-url', "https://github.com/M-logique/Proxies");
--- server/routers/v2ray.js
+++ server/routers/v2ray.js
@@ -2,7 +2,6 @@ const express = require('express')
 const fs = require('node:fs');
 const path = require('node:path')
 const router = express.Router()
-const config = require('../../config.json');
 const utils = require('../functions/utils');
 
 router.get('/:name', async (req, res) => {
@@ -23,7 +22,7 @@ router.get('/:name', async (req, res) => {
     const sliced = protocolFiltered.slice(0, amount);
     const joined = sliced.join('\n\n');
 
-    utils.setHeaders(res, `Git: M-logique/Proxies | ${req.params.name.toUpperCase().replaceAll("-", " ")}`);
+    utils.setHeaders(res, `Github: M-logique/Proxies | ${req.params.name.toUpperCase().replaceAll("-", " ")}`);
 
     if (req.query.decrypted == '' || req.query.decrypted) {
         res.status(200).send(joined);
@@ -33,11 +32,44 @@ router.get('/:name', async (req, res) => {
     }    
 });
 
+router.get('/location/:location', async (req, res) => {
+    let json = require('../../proxies/byLocation.json')
+    json = Object.assign({}, json.profilesByCountryCode, json.profilesByCountryName)
+    json["Netherlands"] = [...json["Netherlands"], ... json["The Netherlands"]]
+    json = lowerize(json)
+
+    const loc = req.params.location;
+
+    const configs = json[loc]
+
+    if (!configs) {
+        return res.status(404).send({ error: 'File not found' });
+    }
+    
+    const protocolFiltered = configs.filter(config => !req.query.protocol || config.startsWith(req.query.protocol));
+    const amount = Number(req.query.amount) || Number(req.query.limit) || Number(req.query.count) || -1;
+    const sliced = protocolFiltered.slice(0, amount);
+    const joined = sliced.join('\n\n');
+
+    utils.setHeaders(res, `Github: M-logique/Proxies | ${req.params.location.toUpperCase().replaceAll("-", " ")}`);
+
+    if (req.query.decrypted == '' || req.query.decrypted) {
+        res.status(200).send(joined);
+    } else {
+        const encrypted = utils.b64encode(joined);
+        res.status(200).send(encrypted);
+    }    
+
+})
+
 router.get('/', async (req,res) => {
     const folderPath = path.join(process.cwd(), 'proxies', 'v2ray')
     const v2rayFiles = fs.readdirSync(folderPath);
     const mapped = v2rayFiles.map(file => file.split('.')[0] || file);
-    const joined = mapped.join(', ');
+    const json = require('../../proxies/byLocation.json')
+    const arr = [...json.locations.byCountryCode, ...json.locations.byNames].map(item => 'location/'+item)
+    const all = [...mapped, ...arr]
+    const joined = all.join(', ');
     res.status(200).send(`available endpoints: ${joined}`);
 });
 
@@ -45,3 +77,9 @@ module.exports = {
     path: '/proxies/v2ray',
     router: router
 }
+
+const lowerize = (obj) =>
+    Object.keys(obj).reduce((acc, k) => {
+      acc[k.toLowerCase()] = obj[k];
+      return acc;
+}, {});
