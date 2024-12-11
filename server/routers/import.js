7dc0cb6f0c2875be139a1a6c1e7a86c175e862fd
M-logique <lmy615730@gmail.com>
2024-12-11 10:30:54 +0330

    Create import.js

---
+++ server/routers/import.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { path } = require("./main");
+const router = express.Router();
+
+
+router.get("/import/:app/:pattern/", async (req, res) => {
+
+    let app = req.params.app;
+    let pattern = req.params.pattern;
+    var url = req.query.url;
+
+    if (!url) return res
+        .status(400)
+        .send("bad usage");
+
+    url = url.replace("@", req.headers.host);
+
+    return res.status(301).redirect(`${app}://${pattern}?url=${url}`);
+
+});
+
+router.get("/raw-import", async (req, res) => {
+
+    var url = req.query.url;
+
+    if (!url) return res
+        .status(400)
+        .send("bad usage");
+
+    url = url.replace("@", req.headers.host);
+
+    return res.status(301).redirect(url);
+
+});
+
+
+module.exports = {
+    path: "/",
+    router: router,
+};
