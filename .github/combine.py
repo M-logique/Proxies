2d2522636cbaa82362ea6cbd434f15cc142e0d09
M. logique <100792944+M-logique@users.noreply.github.com>
2025-04-24 21:26:42 +0000

    Add combine

---
+++ .github/combine.py
@@ -0,0 +1,41 @@
+import json
+from glob import glob
+from collections import defaultdict
+
+files = sorted(glob("byLocations/byLocation-*.json"))
+
+final_dict = {
+    "totalProfiles": 0,
+    "locations": {
+        "totalCountries": 0,
+        "byNames": set(),
+        "byCountryCode": set()
+    },
+    "profilesByCountryCode": defaultdict(list),
+    "profilesByCountryName": defaultdict(list)
+}
+
+for file in files:
+    with open(file, "r") as f:
+        data = json.load(f)
+
+        final_dict["totalProfiles"] += data.get("totalProfiles", 0)
+
+        final_dict["locations"]["byNames"].update(data["locations"]["byNames"])
+        final_dict["locations"]["byCountryCode"].update(data["locations"]["byCountryCode"])
+
+        for k, urls in data.get("profilesByCountryCode", {}).items():
+            final_dict["profilesByCountryCode"][k].extend(urls)
+
+        for k, urls in data.get("profilesByCountryName", {}).items():
+            final_dict["profilesByCountryName"][k].extend(urls)
+
+final_dict["locations"]["byNames"] = list(final_dict["locations"]["byNames"])
+final_dict["locations"]["byCountryCode"] = list(final_dict["locations"]["byCountryCode"])
+final_dict["locations"]["totalCountries"] = len(final_dict["locations"]["byCountryCode"])
+
+final_dict["profilesByCountryCode"] = dict(final_dict["profilesByCountryCode"])
+final_dict["profilesByCountryName"] = dict(final_dict["profilesByCountryName"])
+
+with open("byLocations/merged.json", "w") as f:
+    json.dump(final_dict, f, indent=2)
